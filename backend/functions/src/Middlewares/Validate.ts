9046daacb0d212a3706fe8ce83cd23b04feb2091
Gabriel Braz <gabrielbrazcs@gmail.com>
2023-05-12 01:38:34 -0300

    Validate Fucntion completed

--- backend/functions/src/Middlewares/Validate.ts
+++ backend/functions/src/Middlewares/Validate.ts
@@ -2,31 +2,36 @@ import { z } from 'zod';
 import { fromZodError } from 'zod-validation-error';
 import { Request, Response, NextFunction } from 'express';
 
+import { Error400, Error500 } from '@Interfaces/Errors';
+
 type Zod = z.ZodTypeAny;
+type Data = 'body' | 'query' | 'params';
 type Middleware = (req: Request, res: Response, next: NextFunction) => void;
-type Factory = <T extends Zod>(schemas: T) => Middleware;
+type Factory = <T extends Zod>(schemas: T, data: Data) => Middleware;
 
-const Validate: Factory = <T extends Zod>(schema: T) => (req, res, next) => {
+const Validate: Factory = <T extends Zod>(schema: T, data: Data) => (req, res, next) => {
 	try {
-		const parse = schema.parse(req.body);
+		const parse = schema.parse(req[data]);
 
-		res.locals = { body: parse };
+		res.locals[data] = parse;
 		next();
 	} catch (error) {
 		if (error instanceof z.ZodError) {
 			const { details } = fromZodError(error);
 
-			res.status(400).send({
-				success: false,
+			const response: Error400 = {
 				error: fromZodError(error).name,
-				errors: details,
-			});
+				issues: details,
+			};
+
+			res.status(400).send(response);
 		} else {
-			res.status(500).send({
-				success: false,
+			const response: Error500 = {
 				error: 'Unknown',
 				message: 'Please, file a bug report so we are able to investigate further on this matter.',
-			});
+			};
+
+			res.status(500).send(response);
 		}
 	}
 };
--- backend/functions/src/Router/routes.ts
+++ backend/functions/src/Router/routes.ts
@@ -3,11 +3,11 @@ import { Router } from 'express';
 import { Validate } from '@Middlewares/Validate';
 
 import { HelloController } from '@Controllers/Hello';
-import { GameCtrl, GameSchemas } from '@Controllers/Game';
+import { GameController, GameSchemas, GameFunctions } from '@Controllers/Game';
 
 const router = Router();
 
 router.get('/', HelloController);
-router.post('/game', Validate(GameSchemas.Generate.Body), GameCtrl.Generate);
+router.post('/game', Validate(GameSchemas.Generate.Body, 'body'), GameFunctions.Generate.BodyValues, GameController.Generate);
 
 export default router;
